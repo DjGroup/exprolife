2b898645ca24b0a0e94bbfbecd9c8d1b03068067
Farshad1428 <farshad_abd@hotmail.com>
2013-11-05 16:51:58 +0330

    competence db added

--- exprolife/social/models.py
+++ exprolife/social/models.py
@@ -11,3 +11,16 @@ class User(models.Model):
     #male=0 female=1
     sex = models.BooleanField(blank=False, default=None)
     #image = models.ImageField()
+
+    
+class Competence(models.Model):
+    title = models.CharField(max_length=30, blank=False, default=None)
+    description = models.TextField(max_length=100, blank=True, default=None)
+    tags = models.CharField(max_length=50, blank=False, default=None)
+    developers = models.CharField(max_length=50, blank=False, default=None)
+    manager = models.CharField(max_length=20, blank=False, default=None)
+    picture = models.ImageField(upload_to="uploads_image/" ,blank=True)
+    releaseDate = models.DateField()
+    sourceCode = models.FileField(upload_to="uploads_file/",blank=True)
+    usage = models.CharField(max_length=50, blank=True, default=None)
+
